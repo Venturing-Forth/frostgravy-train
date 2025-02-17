d9372268e66a5925f625a544dd3af80989170467
VenturingForth <joshua@codeadventure.uk>
2025-02-17 22:39:10 +0000

    issue-001: added permanent injury enum

---
+++ FrostgravyTrain.Host/PermanentInjury.cs
@@ -0,0 +1,14 @@
+﻿namespace FrostgravyTrain.Host;
+
+public enum PermanentInjury
+{
+    LostToes = 1,
+    SmashedLeg = 2,
+    CrushedArm = 3,
+    LostFingers = 4,
+    NeverQuiteAsStrong = 5,
+    PsychologicalScars = 6,
+    NigglingInjury = 7,
+    SmashedJaw = 8,
+    LostEye = 9
+}
