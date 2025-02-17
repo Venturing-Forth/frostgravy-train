5094b71ec906c4b0b470ab61fd00911ca9be1102
VenturingForth <joshua@codeadventure.uk>
2025-02-17 21:51:04 +0000

    issue-001: added scalar to Nuget packages

---
+++ FrostgravyTrain.Host/Controllers/UsersController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FrostgravyTrain.Host.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+
+public class UsersController : ControllerBase
+{
+    [HttpGet]
+    [Route("users/{userId}/warbands")]
+
+    public ServerResult GetUserWarbands(int userId)
+    {
+        return new ServerResult { };
+    }
+}
