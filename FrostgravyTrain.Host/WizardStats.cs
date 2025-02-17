a9054aae94092923a759a01aa81ffbde32b411f9
VenturingForth <joshua@codeadventure.uk>
2025-02-17 22:29:22 +0000

    issue-001: added wizard stats class

---
+++ FrostgravyTrain.Host/WizardStats.cs
@@ -0,0 +1,11 @@
+﻿namespace FrostgravyTrain.Host;
+
+public class WizardStats
+{
+    public int Move { get; set; }
+    public int Fight { get; set; }
+    public int Shoot { get; set; }
+    public int Armour { get; set; }
+    public int Will { get; set; }
+    public int Health { get; set; }
+}
