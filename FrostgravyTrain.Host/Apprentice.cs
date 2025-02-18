068affdb35977d06dff1a494258de410aa202c20
VenturingForth <joshua@codeadventure.uk>
2025-02-18 06:57:29 +0000

    issue-001: added Soldier class

--- FrostgravyTrain.Host/Apprentice.cs
+++ FrostgravyTrain.Host/Apprentice.cs
@@ -4,7 +4,7 @@ public class Apprentice
 {
     public string Name { get; set; }
     public WizardStats Stats { get; set; }
-    public List<Item> ItemSlots { get; set; };
+    public List<Item> ItemSlots { get; set; }
     public List<PermanentInjury> PermanentInjuries { get; set; }
     public string Notes { get; set; }
 }
--- FrostgravyTrain.Host/Soldier.cs
+++ FrostgravyTrain.Host/Soldier.cs
@@ -2,4 +2,36 @@
 
 public class Soldier
 {
+    public string Name { get; set; }
+    public SoldierType Type { get; set; }
+    public SoldierGrade Grade { get; set; }
+    public WizardStats Stats { get; set; }
+    public int Cost { get; set; }
+    public Item ItemSlot { get; set; }
+    public List<string> Notes { get; set; }
+}
+
+public enum SoldierType
+{
+    Thug = 1,
+    Thief = 2,
+    WarHound = 3,
+    Infantryman = 4,
+    ManAtArms = 5,
+    Apothecary = 6,
+    Archer = 7,
+    Crossbowman = 8,
+    TreasureHunter = 9,
+    Tracker = 10,
+    Knight = 11,
+    Templar = 12,
+    Ranger = 13,
+    Barbarian = 14,
+    Marksman = 15,
+}
+
+public enum SoldierGrade
+{
+    Standard = 1,
+    Specialist = 2,
 }
